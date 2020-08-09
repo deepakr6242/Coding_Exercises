7a5513c0cc020fe6d7cbed213ae4941dccdef702
deepakr6242 <deepakr6242@gmail.com>
2020-08-09 14:55:15 +0530

    Add files via upload

---
+++ DetectCapital.py
@@ -0,0 +1,11 @@
+def detectCapitalUse( word):
+
+		if  word.istitle() == True:
+			return True
+		elif all([True if i.islower() else False for i in word  ])==True:
+			return True
+		elif all([True if i.isupper() else False for i in word  ])==True:
+			return True
+		else:
+			return False
+print(detectCapitalUse('FLAG'))
