9f8674a229b167fb1a838540f065baa0c099a29a
deepakr6242 <deepakr6242@gmail.com>
2020-08-09 16:12:29 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,26 @@
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+
+
+import string
+
+def isPalindrome(s):
+        for i in s:
+			if i in string.punctuation:
+				s = s.replace(i,'')
+        sub = s.replace(' ','').lower()
+        return sub[::-1]==sub[:]
+
+print(isPalindrome("A man, a plan, a canal: Panama"))
+
+
